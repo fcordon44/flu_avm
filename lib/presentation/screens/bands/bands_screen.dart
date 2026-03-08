6a2d42eaa11127303c5feeb2006c364a03c98f50
Fernando Cordón <fcordon@pin.upv.es>
2026-03-08 14:51:49 +0100

    StateNotifierProvider para las bandas

--- lib/presentation/screens/bands/bands_screen.dart
+++ lib/presentation/screens/bands/bands_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flu_avm/config/config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import '../../providers/providers.dart';
 
-class BandsScreen extends StatelessWidget {
+
+
+class BandsScreen extends ConsumerWidget {
   const BandsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final bands = ref.watch(bandsProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Bandas'),
@@ -15,32 +22,46 @@ class BandsScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: bands.length,
         itemBuilder:(context, i) {
-          return _bandTile(bands[i]);
+          return _bandTile(context, ref, bands[i]);
         },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => addereNovumBand(context),
+        onPressed: () => addereNovumBand(context, ref),
       ),
     );
   }
 
 
-  ListTile _bandTile(Band band) {
-    return ListTile(
-      leading: CircleAvatar(
-        child: Text(band.nomen.substring(0, 2).toUpperCase()),
-      ),
-      title: Text(band.nomen),
-      trailing: Text('${ band.numerusVotum }', style: TextStyle(fontSize: 20),),
-      onTap: () {
-        // ignore: avoid_print
-        print(band.nomen);
+ Widget _bandTile(BuildContext context, WidgetRef ref, Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        ref.read(bandsProvider.notifier).delereBand(band);
       },
+      background: Container(
+        padding: EdgeInsets.only(left: 8.0),
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text('Delete Band', style: TextStyle(color: Colors.white),),
+        ),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(band.nomen.substring(0, 2).toUpperCase()),
+        ),
+        title: Text(band.nomen),
+        trailing: Text('${ band.numerusVotum }', style: TextStyle(fontSize: 20),),
+        onTap: () {
+          ref.read(bandsProvider.notifier).addereVotum(band);
+        },
+      ),
     );
   }
 
-  addereNovumBand(BuildContext context) {
+  addereNovumBand(BuildContext context, WidgetRef ref) {
 
     final TextEditingController textumController = TextEditingController();
     
@@ -79,7 +100,7 @@ class BandsScreen extends StatelessWidget {
               isDefaultAction: true,
               child: const Text('Add'),
               onPressed: () {
-                addereBandAdCollectione(context, textumController.text);
+                addereBandAdCollectione(context, ref, textumController.text);
                 context.pop();
               }
             ),
@@ -94,9 +115,16 @@ class BandsScreen extends StatelessWidget {
 
   }
 
-  void addereBandAdCollectione(BuildContext context, String nomen) {
-    print(nomen);
-    context.pop();
+  void addereBandAdCollectione(BuildContext context, WidgetRef ref, String nomen) {
+    
+    if (nomen.length > 1) {
+      ref.read(bandsProvider.notifier).addereBand(
+        Band(
+          id: DateTime.now().toString(), 
+          nomen: nomen, 
+          numerusVotum: 0
+        ));
+    }
   }
 
 }
